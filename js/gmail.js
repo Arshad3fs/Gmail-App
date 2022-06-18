6698cf190de972fdf6cf85d6d5f42d9d624c4181
Arshad Khan Patan <arshad3fs@gmail.com>
2022-06-18 20:05:24 +0530

    Final changes Filter funcationality implementation

---
+++ js/gmail.js
@@ -0,0 +1,21 @@
+function filterEmails(event){
+    const filterText = event.target.value.toLowerCase();
+    
+    if(!filterText || filterText.trim() === '')
+        window.location.reload();    
+
+    const tableRows = document.getElementsByTagName("tr");
+
+    console.log(tableRows.item, typeof tableRows)
+    if(tableRows) {
+
+        for(let i=0; i<tableRows.length; i++){            
+            const tableRow = tableRows[i];
+            if(!tableRow.getAttribute("data-email").toLowerCase().startsWith(filterText)){
+                tableRow.style.display = "none";
+            } else {
+                tableRow.style.display = "block";
+            }
+        }        
+    }
+}
