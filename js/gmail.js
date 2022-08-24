6cf8a385a11ea92daf074c49510299a910123643
Arshad Khan Patan <arshad3fs@gmail.com>
2022-08-24 20:13:41 +0530

    filter panel

--- js/gmail.js
+++ js/gmail.js
@@ -19,6 +19,35 @@ function filterEmails(event){
     }
 }
 
-function moreFilters() {
-    document.createElement('section')
+function moreFilters() {    
+    const expPanel = document.querySelector("#search-container .expansion-panel");
+    if(expPanel){
+       expPanel.remove();
+    } else {
+        const divContainerEl = document.createElement('div');
+        divContainerEl.className = "expansion-panel";
+
+        addField('From', divContainerEl);
+        addField('To', divContainerEl);
+        addField('Subject', divContainerEl);
+
+        document.getElementById('search-container').appendChild(divContainerEl);
+    }
+}
+
+function addField(label, divContainerEl){
+    const container = document.createElement('div');
+    container.className = 'row';
+
+    const labelEl = document.createElement('label');
+    labelEl.innerHTML = label;
+    labelEl.className = 'label';
+
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.className = 'input-field';
+
+    container.appendChild(labelEl);
+    container.appendChild(input);
+    divContainerEl.appendChild(container);
 }
